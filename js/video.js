fb1b1e1e17c373de162dca5250e86ab5ee4ebc40
myancich <59946332+myancich@users.noreply.github.com>
2022-04-14 14:41:50 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,6 +1,8 @@
-var video;
+var video = document.getElementById("myVideo").autoplay = false.loop = false;
+
 
 window.addEventListener("load", function() {
+	video = document.querySelector("#myVideo");
 	console.log("Good job opening the window")
 
 });
@@ -8,4 +10,66 @@ window.addEventListener("load", function() {
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	let vol = document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	// console.log("Pause Video"); not sure if necessary 
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.95;
+	console.log("New Speed is now: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.95;
+	console.log("New Speed is now: " + video.playbackRate);
+}); 
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 15 > video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 15;
+	}
+	console.log("Current time is now at " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	var muted = video.muted; 
+	if (muted == false) {
+		video.muted = true; 
+		console.log("Video Muted");
+		this.innerHTML = "UnMute";
+	}
+	else {
+		video.muted = false; 
+		console.log("Video Unmuted");
+		this.innerHTML = "Mute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function(event) {
+	video.volume = event.currentTarget.value / 100;
+	console.log("Volume is " + video.volume);
+	document.querySelector('#volume').innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.className = "oldSchool";
+	console.log("Video is now old school");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.className = "video";
+	console.log("Video is now original")
+});
+
 
